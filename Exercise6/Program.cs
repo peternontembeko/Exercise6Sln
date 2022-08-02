63b6cb742a3dba5fc22554a747e5b60a5a22d8c9
peternontembeko <peternontembeko22@gmail.com>
2022-08-02 15:42:39 +0200

    Add project files.

---
+++ Exercise6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercise6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "";
+            int i;
+            Console.WriteLine("Enter a string");
+            str1 = Console.ReadLine();
+
+            string[] sep = str1.Split(' ');
+            string mystring = string.Empty;
+
+            for (i = 0; i < sep.Length; i++)
+            {
+                if (mystring.Length < sep[i].Length)
+                {
+                    mystring = sep[i];
+                }
+
+
+            }
+            Console.WriteLine($"\nThe Longest word is:(mystring)");
+            Console.WriteLine();
+        }
+    }
+}
